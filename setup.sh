2d80b3decfbda063bd12545c06edb63a15ed8987
Oliver Gould <ver@buoyant.io>
2020-05-19 22:12:53 +0000

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eux
+set -eu
+set -x
 
 # Requires:
 #
@@ -10,33 +11,27 @@ set -eux
 
 ## Clusters
 
-for cluster in multi0 multi1 ; do
+org_domain="${ORG_DOMAIN:-k3d.example.com}"
+
+PORT=6440
+for cluster in east west ; do
     if k3d get cluster "$cluster" >/dev/null 2>&1 ; then
-        k3d delete cluster "$cluster"
+        echo "Already exists: $cluster" >&2
+        exit 1
     fi
+
+    port="$((PORT++))"
+    k3d create cluster $cluster \
+        --network=multicluster-example \
+        --k3s-server-arg="--cluster-domain=$cluster.${org_domain}" \
+        --wait \
+        --api-port="$port"
 done
 
-org_domain="${ORG_DOMAIN:-k3d.olix0r.net}"
-
-# Start two clusters in k3d, each serving on a different localhost port.
-k3d create cluster multi0 --wait \
-    --network k3d-multi \
-    --k3s-server-arg="--cluster-domain=multi0.${org_domain}" \
-    -a 6443
-k3d create cluster multi1 --wait \
-    --network k3d-multi \
-    --k3s-server-arg="--cluster-domain=multi1.${org_domain}" \
-    -a 7443
-   
-# if CLEANUP is set, delete everything when the script completes.
-if [ -n "${CLEANUP:-}" ]; then
-    trap '{ k3d delete cluster multi0 multi1 ; }' EXIT
-fi
-
 # Load k3d contexts into kubectl.
-k3d get kubeconfig multi0 multi1
+k3d get kubeconfig east west
 
-cadir=$(mktemp -d multicluster.XXXXX)
+cadir=$(mktemp --tmpdir="${TMPDIR:-/tmp}" -d k3d-ca.XXXXX)
 
 # Generate the trust roots. These never touch the cluster. In the real world
 # we'd squirrel these away in a vault.
@@ -46,7 +41,7 @@ step certificate create \
     --profile root-ca \
     --no-password  --insecure --force
 
-for cluster in multi0 multi1 ; do
+for cluster in east west ; do
     domain="${cluster}.${org_domain}"
     ctx="k3d-${cluster}"
 
@@ -59,7 +54,7 @@ for cluster in multi0 multi1 ; do
     key="${cadir}/${cluster}-issuer.key"
     step certificate create "identity.linkerd.${domain}" \
         "$crt" "$key" \
-        --ca=ca.crt --ca-key=ca.key \
+        --ca="$cadir/ca.crt" --ca-key="$cadir/ca.key" \
         --profile=intermediate-ca \
         --not-after 8760h --no-password --insecure --force
 
@@ -81,88 +76,52 @@ done
 #
 # We need to use alternate ports to avoid conflicting with k3d's traefik
 # instance.
-kubectl --context=k3d-multi1 create ns linkerd-multicluster
-kubectl --context=k3d-multi1 annotate ns linkerd-multicluster \
-    'config.linkerd.io/proxy-log-level=linkerd=debug,warn'
-linkerd --context=k3d-multi1 cluster setup-remote \
-    --incoming-port=4180 \
-    --probe-port=4181 \
-    | kubectl --context=k3d-multi1 apply -f -
+for cluster in east west ; do
+    kubectl --context="k3d-$cluster" create ns linkerd-multicluster
+    linkerd --context="k3d-$cluster" cluster setup-remote \
+        --incoming-port=4180 \
+        --probe-port=4181 \
+        | kubectl --context="k3d-$cluster" apply -f -
+done
 
-# Create a local service-mirror namespace.
-kubectl --context=k3d-multi0 create ns linkerd-multicluster
-kubectl --context=k3d-multi0 annotate ns linkerd-multicluster
-    'linkerd.io/inject=enabled' \
-    'config.linkerd.io/proxy-log-level=linkerd=trace,warn'
-
-# Generate credentials so the service-mirror can access multi1's service api.
+# Generate credentials so the service-mirror can access west's service api.
 #
 # Unfortunately, the credentials have the API server IP as addressed from
 # localhost and not the docker network, so we have to patch that up.
-secret1=$(linkerd --context=k3d-multi1 cluster get-credentials \
-     --cluster-name=multi1 \
-     --remote-cluster-domain="multi1.${org_domain}")
-# Grab the LB IP of multi1's API server & replace it in the secret blob:
-lbip1=$(kubectl --context=k3d-multi1 \
+east_secret=$(linkerd --context=k3d-east cluster get-credentials \
+     --cluster-name=east \
+     --remote-cluster-domain="east.${org_domain}")
+# Grab the LB IP of west's API server & replace it in the secret blob:
+east_lb_ip=$(kubectl --context=k3d-east \
     get svc -n kube-system traefik \
     -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
-kubeconfig1=$(echo "$secret1" \
+east_config=$(echo "$east_secret" \
     | sed -ne 's/^  kubeconfig: //p'  | base64 -d \
-    | sed -e "s|https://0.0.0.0:7443|https://${lbip1}:6443|"  | base64 -w0)
-echo "$secret1" \
-    | sed -e "s/^  kubeconfig: .*/  kubeconfig: $kubeconfig1/" \
-    | kubectl --context=k3d-multi0 apply -n linkerd-multicluster -f -
-linkerd --context=k3d-multi0 install-service-mirror \
+    | sed -e "s|https://0.0.0.0:7443|https://${east_lb_ip}:6443|"  | base64 -w0)
+
+west_secret=$(linkerd --context=k3d-west cluster get-credentials \
+     --cluster-name=west \
+     --remote-cluster-domain="west.${org_domain}")
+# Grab the LB IP of west's API server & replace it in the secret blob:
+west_lb_ip=$(kubectl --context=k3d-west \
+    get svc -n kube-system traefik \
+    -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
+west_config=$(echo "$west_secret" \
+    | sed -ne 's/^  kubeconfig: //p'  | base64 -d \
+    | sed -e "s|https://0.0.0.0:7443|https://${west_lb_ip}:6443|"  | base64 -w0)
+
+echo "$east_secret" \
+    | sed -e "s/^  kubeconfig: .*/  kubeconfig: $east_config/" \
+    | kubectl --context=k3d-west apply -n linkerd-multicluster -f -
+linkerd --context=k3d-west install-service-mirror \
     --namespace=linkerd-multicluster \
     --log-level=debug \
-    | kubectl --context=k3d-multi0 apply -f -
+    | kubectl --context=k3d-west apply -f -
 
-# Create the services in both clusters.
-for ctx in k3d-multi0 k3d-multi1 ; do
-    linkerd --context="$ctx" inject example-servers.yml \
-        | kubectl --context="$ctx" apply -f -
-    #kubectl --context=k3d-multi0 annotate ns example \
-    #    'config.linkerd.io/proxy-log-level=linkerd=debug,warn'
-done
-
-# Export the services from multi1 to multi0
-kubectl --context=k3d-multi1 get -n multicluster-test svc -o yaml \
-    | linkerd cluster export-service - \
-    | kubectl --context=k3d-multi1 apply -f -
-
-# Check that the services are mirrored into multi0
-sleep 1
-kubectl --context=k3d-multi0 get -n example svc -o wide
-kubectl --context=k3d-multi0 get -n example ep -o wide \
-    http-multi1  grpc-multi1
-
-kubectl --context=k3d-multi0 apply -f - <<EOF
---
-apiVersion: split.smi-spec.io/v1alpha1
-kind: TrafficSplit
-metadata:
-    name: http
-spec:
-    service: http
-    backends:
-        - service: http
-          weight: 500m
-        - service: http-multi1
-          weight: 500m
---
-apiVersion: split.smi-spec.io/v1alpha1
-kind: TrafficSplit
-metadata:
-    name: grpc
-spec:
-    service: grpc
-    backends:
-        - service: grpc
-          weight: 500m
-        - service: gprc-multi1
-          weight: 500m
-EOF
-
-kubectl --context=k3d-multi0 run \
-    --image=buoyantio/bb:v0.0.5 \
-    
+echo "$west_secret" \
+    | sed -e "s/^  kubeconfig: .*/  kubeconfig: $west_config/" \
+    | kubectl --context=k3d-east apply -n linkerd-multicluster -f -
+linkerd --context=k3d-east install-service-mirror \
+    --namespace=linkerd-multicluster \
+    --log-level=debug \
+    | kubectl --context=k3d-east apply -f -
