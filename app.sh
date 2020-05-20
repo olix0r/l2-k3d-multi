77ff712c33044f69981c97c0ead9597bfc83d5e7
Oliver Gould <ver@buoyant.io>
2020-05-20 00:05:48 +0000

    wip

---
+++ app.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+kubectl --context=k3d-east kustomize ./east |
+    linkerd --context=k3d-east inject - |
+    linkerd --context=k3d-east cluster export-service - --gateway-namespace=linkerd-multicluster |
+    kubectl --context=k3d-east apply -f -
+
+kubectl --context=k3d-west kustomize ./west |
+    linkerd --context=k3d-west inject - |
+    linkerd --context=k3d-west cluster export-service - --gateway-namespace=linkerd-multicluster |
+    kubectl --context=k3d-west apply -f -
--- setup.sh
+++ setup.sh
@@ -11,117 +11,95 @@ set -x
 
 ## Clusters
 
-org_domain="${ORG_DOMAIN:-k3d.example.com}"
+export ORG_DOMAIN="${ORG_DOMAIN:-k3d.example.com}"
 
-PORT=6440
+CA_DIR=$(mktemp --tmpdir="${TMPDIR:-/tmp}" -d k3d-ca.XXXXX)
+
+# Generate the trust roots. These never touch the cluster. In the real world
+# we'd squirrel these away in a vault.
+step certificate create \
+    "identity.linkerd.${ORG_DOMAIN}" \
+    "$CA_DIR/ca.crt" "$CA_DIR/ca.key" \
+    --profile root-ca \
+    --no-password  --insecure --force
+
+port=6440
 for cluster in east west ; do
     if k3d get cluster "$cluster" >/dev/null 2>&1 ; then
         echo "Already exists: $cluster" >&2
         exit 1
     fi
 
-    port="$((PORT++))"
-    k3d create cluster $cluster \
+    k3d create cluster "$cluster" \
         --network=multicluster-example \
-        --k3s-server-arg="--cluster-domain=$cluster.${org_domain}" \
+        --k3s-server-arg="--cluster-domain=$cluster.${ORG_DOMAIN}" \
         --wait \
-        --api-port="$port"
-done
+        --api-port="$((port++))"
 
-# Load k3d contexts into kubectl.
-k3d get kubeconfig east west
-
-cadir=$(mktemp --tmpdir="${TMPDIR:-/tmp}" -d k3d-ca.XXXXX)
-
-# Generate the trust roots. These never touch the cluster. In the real world
-# we'd squirrel these away in a vault.
-step certificate create \
-    "identity.linkerd.${org_domain}" \
-    "$cadir/ca.crt" "$cadir/ca.key" \
-    --profile root-ca \
-    --no-password  --insecure --force
-
-for cluster in east west ; do
-    domain="${cluster}.${org_domain}"
-    ctx="k3d-${cluster}"
+    k3d get kubeconfig "$cluster"
 
     # Check that the cluster is up and running.
-    while ! linkerd --context="$ctx" check --pre ; do sleep 2 ; done
+    while ! linkerd --context="k3d-$cluster" check --pre ; do :; done
 
     # Create issuing credentials. These end up on the cluster (and can be
     # rotated from the root).
-    crt="${cadir}/${cluster}-issuer.crt"
-    key="${cadir}/${cluster}-issuer.key"
+    crt="${CA_DIR}/${cluster}-issuer.crt"
+    key="${CA_DIR}/${cluster}-issuer.key"
+    domain="${cluster}.${ORG_DOMAIN}"
     step certificate create "identity.linkerd.${domain}" \
         "$crt" "$key" \
-        --ca="$cadir/ca.crt" --ca-key="$cadir/ca.key" \
+        --ca="$CA_DIR/ca.crt" \
+        --ca-key="$CA_DIR/ca.key" \
         --profile=intermediate-ca \
-        --not-after 8760h --no-password --insecure --force
+        --not-after 8760h --no-password --insecure
 
     # Install Linkerd into the cluster.
-    linkerd --context="$ctx" install \
-        --cluster-domain="$domain" \
-        --identity-trust-domain="$domain" \
-        --identity-trust-anchors-file="$cadir/ca.crt" \
-        --identity-issuer-certificate-file="${crt}" \
-        --identity-issuer-key-file="${key}" \
-        | kubectl --context="$ctx" apply -f -
+    linkerd --context="k3d-$cluster" install \
+            --cluster-domain="$domain" \
+            --identity-trust-domain="$domain" \
+            --identity-trust-anchors-file="$CA_DIR/ca.crt" \
+            --identity-issuer-certificate-file="${crt}" \
+            --identity-issuer-key-file="${key}" |
+        kubectl --context="k3d-$cluster" apply -f -
 
     # Wait some time and check that the cluster has started properly.
-    sleep 20
-    while ! linkerd --context="$ctx" check ; do sleep 2 ; done
-done
+    sleep 30
+    while ! linkerd --context="k3d-$cluster" check ; do :; done
 
-# Setup a gateway on the remote cluster.
-#
-# We need to use alternate ports to avoid conflicting with k3d's traefik
-# instance.
-for cluster in east west ; do
+    # Setup a gateway on the remote cluster.
     kubectl --context="k3d-$cluster" create ns linkerd-multicluster
     linkerd --context="k3d-$cluster" cluster setup-remote \
-        --incoming-port=4180 \
-        --probe-port=4181 \
-        | kubectl --context="k3d-$cluster" apply -f -
+            --gateway-namespace=linkerd-multicluster \
+            --service-account-namespace=linkerd-multicluster \
+            --incoming-port=4180 \
+            --probe-port=4181 |
+        kubectl --context="k3d-$cluster" apply -f -
+
+    linkerd --context="k3d-$cluster" install-service-mirror \
+            --namespace=linkerd-multicluster \
+            --log-level=debug |
+        kubectl --context="k3d-$cluster" apply -f -
 done
 
-# Generate credentials so the service-mirror can access west's service api.
+# Generate credentials so the service-mirror
 #
 # Unfortunately, the credentials have the API server IP as addressed from
 # localhost and not the docker network, so we have to patch that up.
-east_secret=$(linkerd --context=k3d-east cluster get-credentials \
-     --cluster-name=east \
-     --remote-cluster-domain="east.${org_domain}")
-# Grab the LB IP of west's API server & replace it in the secret blob:
-east_lb_ip=$(kubectl --context=k3d-east \
-    get svc -n kube-system traefik \
-    -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
-east_config=$(echo "$east_secret" \
-    | sed -ne 's/^  kubeconfig: //p'  | base64 -d \
-    | sed -e "s|https://0.0.0.0:7443|https://${east_lb_ip}:6443|"  | base64 -w0)
+fetch_credentials() {
+    cluster="$1"
+    # Grab the LB IP of cluster's API server & replace it in the secret blob:
+    lb_ip=$(kubectl --context="k3d-$cluster" get svc -n kube-system traefik \
+        -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
+    secret=$(linkerd --context="k3d-$cluster" cluster get-credentials \
+            --cluster-name="$cluster" \
+            --remote-cluster-domain="${cluster}.${ORG_DOMAIN}" \
+            --service-account-namespace=linkerd-multicluster)
+    config=$(echo "$secret" |
+        sed -ne 's/^  kubeconfig: //p' | base64 -d |
+        sed -Ee "s|server: https://.*|server: https://${lb_ip}:6443|" | base64 -w0)
+    # shellcheck disable=SC2001
+    echo "$secret" | sed -e "s/  kubeconfig: .*/  kubeconfig: $config/"
+}
 
-west_secret=$(linkerd --context=k3d-west cluster get-credentials \
-     --cluster-name=west \
-     --remote-cluster-domain="west.${org_domain}")
-# Grab the LB IP of west's API server & replace it in the secret blob:
-west_lb_ip=$(kubectl --context=k3d-west \
-    get svc -n kube-system traefik \
-    -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
-west_config=$(echo "$west_secret" \
-    | sed -ne 's/^  kubeconfig: //p'  | base64 -d \
-    | sed -e "s|https://0.0.0.0:7443|https://${west_lb_ip}:6443|"  | base64 -w0)
-
-echo "$east_secret" \
-    | sed -e "s/^  kubeconfig: .*/  kubeconfig: $east_config/" \
-    | kubectl --context=k3d-west apply -n linkerd-multicluster -f -
-linkerd --context=k3d-west install-service-mirror \
-    --namespace=linkerd-multicluster \
-    --log-level=debug \
-    | kubectl --context=k3d-west apply -f -
-
-echo "$west_secret" \
-    | sed -e "s/^  kubeconfig: .*/  kubeconfig: $west_config/" \
-    | kubectl --context=k3d-east apply -n linkerd-multicluster -f -
-linkerd --context=k3d-east install-service-mirror \
-    --namespace=linkerd-multicluster \
-    --log-level=debug \
-    | kubectl --context=k3d-east apply -f -
+fetch_credentials east | kubectl --context=k3d-west apply -n linkerd-multicluster -f -
+fetch_credentials west | kubectl --context=k3d-east apply -n linkerd-multicluster -f -
